19e8e36b4030f58b338995885b803554e175426b
Geoff Nightingale <geoffrey.nightingale@contino.io>
2024-02-12 20:22:31 +0000

    Add more tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,7 @@ def test_cv_webapp_1() -> None:
     assert 1 + 1 == 2
 
 def test_cv_webapp_2() -> None:
-    assert 1 + 1 == 3
+    assert 2 + 2 == 4
 
+def test_cv_webapp_2() -> None:
+    assert 3 + 3 == 1
